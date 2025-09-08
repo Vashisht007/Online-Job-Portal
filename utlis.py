067fd0ab168a925e586c510d3cfdf2121bb66008
Vashisht007 <vashiurgonda@gmail.com>
2025-09-08 19:44:54 +0530

    Delete utlis.py

--- utlis.py
+++
@@ -1,6 +0,0 @@
-ALLOWED_EXTENSIONS = ['png','jpg','jpeg','pdf']
-UPLOADS_FOLDER='uploads/images/'
-
-def file_valid(file):
-    return '.' in file and \
-    file.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
